08c47177b2a82961087835b84726098fabd4f571
James Kyburz <james.kyburz@gmail.com>
2016-09-20 17:16:48 +0200

    fix since form-data updated

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var request = require('hyperquest');
 var FormData = require('form-data');
 var bl = require('bl');
 var JSZip = require('jszip');
@@ -11,10 +10,10 @@ function fontello2jszip(config, cb) {
   if (typeof config !== 'string') config = JSON.stringify(config);
   var fd = new FormData();
   fd.append('config', config, {filename: 'config', content_type: 'application/json'});
-  var post = request.post(fontello, {headers: fd.getCustomHeaders()});
-
-  post.pipe(bl(download));
-  fd.pipe(post);
+  fd.submit(fontello, function (err, res) {
+    if (err || res.statusCode !== 200) return cb('failed to upload to fontello ' + (err || '') + ':' + res.statusCode);
+    res.pipe(bl(download));
+  })
 
   function download(err, session) {
     if (err) return cb(err);
